b612cbdf6cf1054e2d0dbc0bd92ddebd3020c3b3
Fumbi1 <olumuyiwafumbi@gmail.com>
2024-07-03 19:53:53 +0100

    Created the home page and custom animations

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { proxy } from "valtio";
+
+
+//in const state, we define the initial statewe want and use it similar to react context
+const state = proxy({
+    intro: true, // are we currently in the home route?
+    color: '#EFBD48',
+    isLogoTexture: true, //meaining are we currently displaying the logo on ouur shirt?
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './thtreejs.png'
+})
+
+export default state;
